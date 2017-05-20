569574ac9db171f8307f901820b2eaaefe018b37
Kazuhito Higashioka <kmhigashioka@gmail.com>
2017-05-20 09:50:39 +0800

    State of component

--- app/todo.js
+++ app/todo.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 class Todo extends React.Component {
+
+  constructor(props) {
+    super();
+
+    this.state = {
+      done: props.todo.done
+    };
+  }
   
   render() {
     return (
       <div>
         <label>Task :</label>
         <p>{this.props.todo.task}</p>
-        <button type="button" onClick={this.toggleDone.bind(this)}>Set to {this.props.todo.done ? 'Undone' : 'Done'}</button>
+        <button type="button" 
+                onClick={this.toggleDone.bind(this)}>
+          Set to {this.state.done ? 'Undone' : 'Done'}
+        </button>
       </div>
     );
   }
 
   toggleDone() {
-    this.props.todo.done = !this.props.todo.done;
+    this.setState({
+      done: !this.state.done
+    });
   }
   
 }
