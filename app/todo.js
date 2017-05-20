42b9dbb422c129726ad77d7ffc1b82f75e8e4c8b
Kazuhito Higashioka <kmhigashioka@gmail.com>
2017-05-20 10:25:57 +0800

    Added react prop-types

--- app/todo.js
+++ app/todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Todo extends React.Component {
 
@@ -30,5 +31,9 @@ class Todo extends React.Component {
   }
   
 }
+
+Todo.propTypes = {
+  todo: PropTypes.object
+};
 
 export default Todo;
