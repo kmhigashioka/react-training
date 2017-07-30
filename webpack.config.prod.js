d9944abb235b04deb886b08cab20ca327c130698
Kazuhito Higashioka <kmhigashioka@gmail.com>
2017-07-30 21:42:23 +0800

    Added webpack prod config

---
+++ webpack.config.prod.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react', 'stage-2'],
+          plugins: ['transform-decorators-legacy']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      cacheFolder: path.resolve(__dirname, 'public/cached_uglify'),
+      debug: false,
+      minimize: true,
+      sourceMap: false,
+      output: {
+        comments: false
+      },
+      compressor: {
+          warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+};
